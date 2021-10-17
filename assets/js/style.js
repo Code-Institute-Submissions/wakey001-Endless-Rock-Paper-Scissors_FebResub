c58fc9ead6ab0f29b1f778a486f3ffdb4a5d74c2
wakey001 <stefanwakeman001@hotmail.co.uk>
2021-10-17 19:31:08 +0000

    added playGame function

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,5 @@
  
- /**Declare constants for DOM elements
+ /** Declare constants for DOM elements
   *  and possible choices */
 
   const buttons = document.getElementsByClassName('control');
@@ -8,4 +8,32 @@
   const  playerImage = document.getElementById('player-image');
   const  computerImage = document.getElementById('computer-image');
   const messages = document.getElementById('messages');
-  const  choices = ('rock', 'paper', 'scissors');
+  const  choices = ['rock', 'paper', 'scissors'];
+
+  /**
+   * Add event listener too all the buttons
+   */
+  for (let button of buttons){
+      button.addEventListener('click', function (){
+          let playerChoice = this.getAttribute('data-choice');
+          playGame(playerChoice);
+      });
+}
+/**
+ * Playgame function with playerChoice parameter ,
+ * shows chosen rock,paper,scissors as an image 
+ */
+function playGame(playerChoice){
+
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result);
+}
