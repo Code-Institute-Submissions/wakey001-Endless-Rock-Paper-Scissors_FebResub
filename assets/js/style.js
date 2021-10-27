2edd9d8bae97647aedc8c1bb23ac3c6989da860d
wakey001 <stefanwakeman001@hotmail.co.uk>
2021-10-27 17:07:49 +0000

    improve syntax , add semi-colons

--- assets/js/style.js
+++ assets/js/style.js
@@ -5,12 +5,11 @@ const playerScoreSpan = document.getElementById('player-score');
 const computerScoreSpan = document.getElementById('computer-score');
 const scoreBoardDiv = document.querySelector('.score-board');
 const computerChoiceDisplay = document.querySelector('#computer-choice');
-const  userChoiceDisplay = document.querySelector('#user-choice');
+const userChoiceDisplay = document.querySelector('#user-choice');
 const resultDisplay = document.querySelector('#result');
 const possibleChoices = document.querySelectorAll('button');
-const playerImage = document.getElementById('player-img');
-const computerImage = document.getElementById('computer-img');
-const choices = ["rock", "paper", "scissors"];
+
+
 let userChoice ;
 let computerChoice ;
 let result ;
@@ -18,64 +17,63 @@ let result ;
 /*Event listener for USER buttons */
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
-userChoice = e.target.id
-userChoiceDisplay.innerHTML = userChoice
-generateComputerChoice()
-getResult()
-}))
+userChoice = e.target.id;
+userChoiceDisplay.innerHTML = userChoice;
+generateComputerChoice();
+getResult();
+}));
 /*Generates computer choice  */
 function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random () *3 +1)
+    const randomNumber = Math.floor(Math.random () *3 +1);
     if (randomNumber === 1){
-        computerChoice = 'rock'
+        computerChoice = 'rock';
     }
     if (randomNumber === 2){
-        computerChoice = 'scissors'
+        computerChoice = 'scissors';
     }
     if(randomNumber === 3){
-        computerChoice = 'paper'
+        computerChoice = 'paper';
     }
-    computerChoiceDisplay.innerHTML = computerChoice
-    computerImage.src = `assets/images/${computerChoice}.png`
-    computerImage.alt = computerChoice;
+    computerChoiceDisplay.innerHTML = computerChoice;
+    
 }
 /* Result to tell you who has won  */
 function getResult(){
     if(computerChoice === userChoice){
-        result = 'This is a Draw!'
+        result = 'This is a Draw!';
     
     }
     if(computerChoice === 'rock' && userChoice === 'paper'){
-        result = 'You Win!'
+        result = 'You Win!';
         win();
 
     }
     if(computerChoice === 'rock' && userChoice === 'scissors'){
-        result = 'You lost!'
+        result = 'You lost!';
         lose();
 
     }
     if(computerChoice === 'paper' && userChoice === 'scissors'){
-        result= 'You Win!'
+        result= 'You Win!';
         win();
         
     }
     if(computerChoice === 'paper' && userChoice === 'rock'){
-        result= 'You lost!'
+        result= 'You lost!';
         lose();
         
     }
     if(computerChoice === 'scissors' && userChoice === 'rock'){
-        result= 'You Win!'
+        result= 'You Win!';
         win();
         
     }
     if(computerChoice === 'scissors' && userChoice === 'paper'){
-        result= 'You lost!'
+        result= 'You lost!';
         lose();
         
     }
-    resultDisplay.innerHTML = result
+    resultDisplay.innerHTML = result;
 
 }
 /**
