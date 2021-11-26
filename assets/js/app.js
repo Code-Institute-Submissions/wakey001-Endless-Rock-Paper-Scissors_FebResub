4cfa37efa151b58199fc7b3c3f3cd17622134b85
wakey001 <stefanwakeman001@hotmail.co.uk>
2021-11-26 13:53:32 +0000

    added function displayuserchoice into lines 18,21,24

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,14 +10,21 @@ const possibleChoices = document.querySelectorAll('button');
 const computerImage = document.querySelector('#house-choice-img');
 let userChoice ;
 let result ;
-const rockRef = document.querySelector('#rock')
-const scissorsRef = document.querySelector('#scissors')
-const paperRef = document.querySelector('#paper')
+const rockRef = document.querySelector('#rock');
+const scissorsRef = document.querySelector('#scissors');
+const paperRef = document.querySelector('#paper');
 
 
-document.querySelector('#rock').addEventListener('click', displayUserChoice);
-document.querySelector('#paper').addEventListener('click', displayUserChoice);
-document.querySelector('#scissors').addEventListener('click', displayUserChoice);
+document.querySelector('#rock').addEventListener('click');{
+    displayUserChoice()};
+
+document.querySelector('#paper').addEventListener('click');{
+    displayUserChoice()};
+
+document.querySelector('#scissors').addEventListener('click');{
+    displayUserChoice()};
+
+
  //Passes the value rock,paper,scissors from html into image 
  
 const controlOptions = {
