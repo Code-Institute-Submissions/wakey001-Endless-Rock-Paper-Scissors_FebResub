b18e83127afe8bdbe8651a34d9d46bdc23eba6fa
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-24 22:42:13 +0000

    added more text to result variable in if statements, not currently working prpoerly

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,8 +41,8 @@ const controlOptions = {
     rock: "./assets/images/rock.png",
     paper: "./assets/images/paper.png",
     scissors: "./assets/images/scissors.png",
-    lizard: "",
-    spock: ""
+    lizard: ".assets/images/lizard.png",
+    spock: ".assets/images/spock.png"
 };
 //Gets the value of rock paper scissor from html buttons
  
@@ -80,102 +80,102 @@ function getResult(){
     
     }
     if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'paper'){
-        result = 'You Win!';
+        result = 'You Win, paper wraps rock !';
         win();
 
     }
     if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'scissors'){
-        result = 'You lost!';
+        result = 'You lost!, rock jams scissors';
         lose();
     
     }
     if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'lizard'){
-        result = 'You lost!';
+        result = 'You lost!, rock crushes lizard';
         lose();
 
     }
     if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'spock'){
-        result = 'You Win!';
+        result = 'You Win!, spock vaporizes rock';
         win();
 
     }
     if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'scissors'){
-        result= 'You Win!';
+        result= 'You Win!, scissors cuts paper';
         win();
         
     }
     if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'rock'){
-        result= 'You lost!';
+        result= 'You lost!, paper wraps rock';
         lose();
         
     }
     if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'lizard'){
-        result= 'You Win!';
+        result= 'You Win!, lizard eats paper';
         win();
         
     }
     if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'spock'){
-        result= 'You lost!';
+        result= 'You lost!, paper disproves spock';
         lose();
         
     }
     if(computerChoiceDisplay.innerHTML === 'scissors' && userChoice === 'rock'){
-        result= 'You Win!';
+        result= 'You Win!, rock jams scissors';
         win();
         
     }
     if(computerChoiceDisplay.innerHTML === 'scissors' && userChoice === 'paper'){
-        result= 'You lost!';
+        result= 'You lost!, scissors cuts paper';
         lose();
         
     }
     if(computerChoiceDisplay.innerHTML === 'scissors' && userChoice === 'lizard'){
-        result= 'You lost!';
+        result= 'You lost! scissors decapitates lizard';
         lose();
         
     }
     if(computerChoiceDisplay.innerHTML === 'scissors' && userChoice === 'spock'){
-        result= 'You Win!';
+        result= 'You Win!, spock smashes scissors';
         Win();
         
     }
     if(computerChoiceDisplay.innerHTML === 'lizard' && userChoice === 'rock'){
-        result= 'You Win!';
+        result= 'You Win!, rock crushes lizard';
         Win();
         
     }
     if(computerChoiceDisplay.innerHTML === 'lizard' && userChoice === 'paper'){
-        result= 'You Lose!';
+        result= 'You Lose!, lizard eats paper';
         lose();
         
     }
     if(computerChoiceDisplay.innerHTML === 'lizard' && userChoice === 'scissors'){
-        result= 'You Win!';
+        result= 'You Win!, scissors decapitates lizard';
         Win();
         
     }
     if(computerChoiceDisplay.innerHTML === 'lizard' && userChoice === 'spock'){
-        result= 'You lose!';
+        result= 'You lose!, lizard poisons spock';
         lose();
         
     }
     if(computerChoiceDisplay.innerHTML === 'spock' && userChoice === 'rock'){
-        result= 'You lose!';
+        result= 'You lose!, spock vaporizes rock';
         lose();
         
     }
     if(computerChoiceDisplay.innerHTML === 'spock' && userChoice === 'paper'){
-        result= 'You Win!';
+        result= 'You Win!, paper disproves spock';
         Win();
         
     }
     if(computerChoiceDisplay.innerHTML === 'spock' && userChoice === 'scissors'){
-        result= 'You lose!';
+        result= 'You lose!, spock smashes scissors';
         lose();
         
     }
     if(computerChoiceDisplay.innerHTML === 'spock' && userChoice === 'lizard'){
-        result= 'You Win!';
+        result= 'You Win!, lizard poisons spocks';
         Win();
         
     }
