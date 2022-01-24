97f3657479600bf4d5a1c0284a5cc7d2e8956c5d
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-24 22:19:37 +0000

    add if statements for lizard and spock

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,8 @@ let result ;
 const rockRef = document.querySelector('#rock');
 const scissorsRef = document.querySelector('#scissors');
 const paperRef = document.querySelector('#paper');
+const lizardRef = document.querySelector('#lizard')
+const spockRef = document.querySelector('#spock')
 
 
 /* Event listeners for the <a> buttons Rock, paper scissors .
@@ -27,13 +29,20 @@ scissorsRef.addEventListener('click', () => {
 paperRef.addEventListener('click', () => {
     displayUserChoice('paper');});
 
+paperRef.addEventListener('click', () => {
+    displayUserChoice('lizard');});
+
+paperRef.addEventListener('click', () => {
+    displayUserChoice('spock');});
 
  //Passes the value rock,paper,scissors from html into image 
  
 const controlOptions = {
     rock: "./assets/images/rock.png",
     paper: "./assets/images/paper.png",
-    scissors: "./assets/images/scissors.png"
+    scissors: "./assets/images/scissors.png",
+    lizard: "",
+    spock: ""
 };
 //Gets the value of rock paper scissor from html buttons
  
@@ -55,7 +64,7 @@ getResult();
 //Generates computer choice . 
 
 function generateComputerChoice(){
-    const choices = ['rock', 'scissors', 'paper'];
+    const choices = ['rock', 'scissors', 'paper', 'lizard', 'spock'];
     const randomNumber = Math.floor(Math.random () * choices.length);
     computerChoiceDisplay.innerHTML = choices[randomNumber] ;
     computerImage.src = `./assets/images/${computerChoiceDisplay.innerHTML}-mirror.png`; 
@@ -78,6 +87,16 @@ function getResult(){
     if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'scissors'){
         result = 'You lost!';
         lose();
+    
+    }
+    if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'lizard'){
+        result = 'You lost!';
+        lose();
+
+    }
+    if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'spock'){
+        result = 'You Win!';
+        win();
 
     }
     if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'scissors'){
@@ -86,6 +105,16 @@ function getResult(){
         
     }
     if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'rock'){
+        result= 'You lost!';
+        lose();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'lizard'){
+        result= 'You Win!';
+        win();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'spock'){
         result= 'You lost!';
         lose();
         
@@ -100,6 +129,57 @@ function getResult(){
         lose();
         
     }
+    if(computerChoiceDisplay.innerHTML === 'scissors' && userChoice === 'lizard'){
+        result= 'You lost!';
+        lose();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'scissors' && userChoice === 'spock'){
+        result= 'You Win!';
+        Win();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'lizard' && userChoice === 'rock'){
+        result= 'You Win!';
+        Win();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'lizard' && userChoice === 'paper'){
+        result= 'You Lose!';
+        lose();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'lizard' && userChoice === 'scissors'){
+        result= 'You Win!';
+        Win();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'lizard' && userChoice === 'spock'){
+        result= 'You lose!';
+        lose();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'spock' && userChoice === 'rock'){
+        result= 'You lose!';
+        lose();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'spock' && userChoice === 'paper'){
+        result= 'You Win!';
+        Win();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'spock' && userChoice === 'scissors'){
+        result= 'You lose!';
+        lose();
+        
+    }
+    if(computerChoiceDisplay.innerHTML === 'spock' && userChoice === 'lizard'){
+        result= 'You Win!';
+        Win();
+        
+    }
+
     resultDisplay.innerHTML = result;
 }
  
